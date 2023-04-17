db225e5d668cf47c7f6d2d60e8fc27334fb324cf
VladDev322 <VladL322@yandex.ru>
2023-04-18 02:49:22 +0300

    feat: continue extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,36 +16,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(/*str, options*/) {
-  throw new NotImplementedError('Not implemented');
-  // let result = ''
-  // if (options.separator === undefined) {
-  //   for (let i = 0; i < options.repeatTimes; i++) {
-  //     result += str
-  //     if (i != options.repeatTimes - 1) result += '+'
-  //   }
-  // }
-  // else if (options.addition === undefined) {
-  //   for (let i = 0; i < options.repeatTimes; i++) {
-  //     result += str
-  //     if (i != options.repeatTimes - 1) result += options.separator
-  //   }
-  // }
-  // else if (options.addition === undefined) {
-  //   for (let i = 0; i < options.repeatTimes; i++) {
-  //     result += str
-  //     if (i != options.repeatTimes - 1) result += options.separator
-  //   }
-  // }
-  // else if (options.additionSeparator === undefined) {
-  //   for (let i = 0; i < options.repeatTimes; i++) {
-  //     result += str
-  //     for (let i = 0; i < options.additionRepeatTimes; i++) {
-  //       result += options.addition
-  //     }
-  //     if (i != options.repeatTimes - 1) result += options.separator
-  //   }
-  // }
-  // return result
+  let result = ''
+  if (options.separator === undefined) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      result += str
+      if (i != options.repeatTimes - 1) result += '+'
+    }
+  }
+  else if (options.addition === undefined) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      result += str
+      if (i != options.repeatTimes - 1) result += options.separator
+    }
+  }
+  else if (options.addition === undefined) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      result += str
+      if (i != options.repeatTimes - 1) result += options.separator
+    }
+  }
+  else if (options.additionSeparator === undefined) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      result += str
+      for (let i = 0; i < options.additionRepeatTimes; i++) {
+        result += options.addition
+      }
+      if (i != options.repeatTimes - 1) result += options.separator
+    }
+  }
+  return result
 }
 
 module.exports = {
